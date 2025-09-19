cf0f350711a76aef8332093882a3669405926ab3
Agostina Vega <agosvega2841@gmail.com>
2025-09-18 21:22:45 -0300

    inicializar el proyecto y agregar la variable amigos

---
+++ app.js
@@ -0,0 +1,6 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+// capturar el valor del campo de entrada
+//validad la entrada, si esta vacio alerta "por favor inserte un nombre"
+//si el nombre es valido almacenarlo
+//limpiar el campo de entrada
