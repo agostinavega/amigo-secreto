4b44e9947076c08ac4ea19b3105a3d09f974ab1b
Agostina Vega <agosvega2841@gmail.com>
2025-09-18 22:26:50 -0300

    se agrego una funcion para visualizar la lista de amigos

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ let amigos = [];
 //validad la entrada, si esta vacio alerta "por favor inserte un nombre"
 //si el nombre es valido almacenarlo
 //limpiar el campo de entrada
+function generarListaAmigos () {
+    const ul = document.getElementById("listaAmigos");
+    ul.innerHTML = "";
+    for (let index = 0; index < amigos.length; index++) {
+       const li = document.createElement("li");
+       li.textContent = amigos[index];
+        ul.appendChild(li)
+    }
+
+}
 function agregarAmigo () {
     const amigo = document.getElementById("amigo").value;
     if (amigo == "") {
-        alert("mmmm")
+        alert("Por favor, inserte un nombre.")
     } else {
         amigos.push(amigo)
         document.getElementById("amigo").value = ""
+        generarListaAmigos()
     }
 };
